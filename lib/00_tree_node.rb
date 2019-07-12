7cbc003c8d47e881da334fe6f86a8caaf7610071
Anthony Garino <Anthonygarino@utexas.edu>
2019-07-12 17:48:54 -0500

    made parent= method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,9 +1,24 @@
 class PolyTreeNode
-  attr_accessor :parent, :children, :value
+  attr_accessor :children, :value
+  attr_reader :parent
   def initialize(value, parent = nil)
     @children = []
     @parent = parent
     @value = value
   end
+
+  def parent=(node)
+    if @parent != nil
+      @parent.children.delete(self)
+    end
+    @parent = node
+    node.children << self unless node.nil?
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+
 
 end
